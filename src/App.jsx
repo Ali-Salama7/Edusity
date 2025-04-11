f45f05f5a12f45fb1a7a37f18af066612a68a8cb
Ali-Salama7 <alysaied2000@gmail.com>
2025-04-11 03:53:34 +0200

    first commit react

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navber/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Tesmonials from './components/Testmonials/Tesmonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import VideoPlayer from './components/videoPlayer/VideoPlayer'
+
+const App = () => {
+
+  const [playState, setPlayState] = useState(false)
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <div className="container">
+      <Title subTitle='Our PROGRAM' title='What We Offer'/>
+      <Programs />
+      <About setPlayState={setPlayState}/>
+      <Title subTitle='Gallery' title='Campus Photos'/>
+      <Campus />
+      <Title subTitle='TESTIMONIALS' title='What Student Says'/>
+      <Tesmonials />
+      <Title subTitle='Contact Us' title='Get in Touch'/>
+      <Contact />
+      <Footer />
+      </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navber/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
+import { Link } from 'react-scroll';
+
+
+const Navbar = () => {
+
+  const [sticky, setSticky] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 500 ? setSticky(true) : setSticky(false)
+    })
+  }, [])
+
+  const [mobileSet, setMobileSet] = useState(false)
+  const toggleClick = () => {
+    mobileSet ? setMobileSet(false) : setMobileSet(true)
+  }
+
+  return (
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+        <img src={logo} alt="" className='logo'/>
+        <ul className={mobileSet ? '' : 'mobile'}>
+            <li><Link to='home' smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+            <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
+            <li><Link to='capmus' smooth={true} offset={-250} duration={500}>Campus</Link></li>
+            <li><Link to='testmonials' smooth={true} offset={-250} duration={500}>Testimonials</Link></li>
+            <li><Link to='contact' smooth={true} offset={-250} duration={500} className="btn">Contact us</Link></li>
+        </ul>
+        <img src={menu_icon} alt=""  className='menu-icon' onClick={toggleClick}/>
+    </nav>
+  )
+}
+
+export default Navbar
+
+//2:19
